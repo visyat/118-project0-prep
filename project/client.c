b594f6829389ff0dc1daa49a245be57be3a5c3d1
Vishal Yathish <vishal.yathish@gmail.com>
2024-12-29 16:13:22 -0800

    send bytes read, not buffer size

--- project/client.c
+++ project/client.c
@@ -3,45 +3,64 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-int main() {
-   /* 1. Create socket */
+// client takes port number and hostname args ...
+// hostname = argv[1], port = argv[2]
+int main(int argc, char *argv[]) {
    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
                   // use IPv4  use UDP
 
-   /* 2. Construct server address */
+   // make socket non-blocking ...
+   int flags_socket = fcntl(sockfd, F_GETFL);
+   flags_socket |= O_NONBLOCK;
+   fcntl(sockfd, F_SETFL, flags_socket);
+
+   // make standard input non-blocking ...
+   int flags_stdin = fcntl(STDIN_FILENO, F_GETFL);
+   flags_stdin |= O_NONBLOCK;
+   fcntl(STDIN_FILENO, F_SETFL, flags_stdin);
+
    struct sockaddr_in serveraddr;
    serveraddr.sin_family = AF_INET; // use IPv4
    serveraddr.sin_addr.s_addr = INADDR_ANY;
-   // Set sending port
-   int SEND_PORT = 8080;
+   
+   // set sending port ...
+   if (strcmp(argv[1], "localhost") == 0) {
+        serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    } else {
+        serveraddr.sin_addr.s_addr = inet_addr(argv[1]);
+    }
+   int SEND_PORT = atoi(argv[2]);
    serveraddr.sin_port = htons(SEND_PORT); // Big endian
 
-   /* 3. Send data to server */
-   char client_buf[] = "Hello world!";
-   int did_send = sendto(sockfd, client_buf, strlen(client_buf), 
-                     // socket  send data   how much to send
-                        0, (struct sockaddr*) &serveraddr, 
-                     // flags   where to send
-                        sizeof(serveraddr));
-   if (did_send < 0) return errno;
-
-   /* 4. Create buffer to store incoming data */
    int BUF_SIZE = 1024;
    char server_buf[BUF_SIZE];
    socklen_t serversize = sizeof(socklen_t); // Temp buffer for recvfrom API
 
-   /* 5. Listen for response from server */
-   int bytes_recvd = recvfrom(sockfd, server_buf, BUF_SIZE, 
-                           // socket  store data  how much
-                              0, (struct sockaddr*) &serveraddr, 
-                              &serversize);
-   // Execution will stop here until `BUF_SIZE` is read or termination/error
-   // Error if bytes_recvd < 0 :(
-   if (bytes_recvd < 0) return errno;
-   // Print out data
-   write(1, server_buf, bytes_recvd);
+   for (;;) {
+      // listen to response from server ...
+      int bytes_recvd = recvfrom(sockfd, server_buf, BUF_SIZE, 
+                              // socket  store data  how much
+                                 0, (struct sockaddr*) &serveraddr, 
+                                 &serversize);
+      if (bytes_recvd <= 0) {
+         if (errno != EAGAIN && errno != EWOULDBLOCK) return errno;
+      }
+      // Print out data
+      write(1, server_buf, bytes_recvd);
 
+      // send messages from stdin ...
+      char client_buf[BUF_SIZE];
+      ssize_t bytes_read = read(STDIN_FILENO, client_buf, BUF_SIZE);
+      int did_send = sendto(sockfd, client_buf, bytes_read, 
+                        // socket  send data   how much to send
+                           0, (struct sockaddr*) &serveraddr, 
+                        // flags   where to send
+                           sizeof(serveraddr));
+      if (did_send < 0) return errno;
+   }
 
    /* 6. You're done! Terminate the connection */     
    close(sockfd);
--- project/server.c
+++ project/server.c
@@ -4,8 +4,9 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
-// server takes port number argument from stdin ...
+// server takes port number argument ...
 int main(int argc, char *argv[]) {
    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -15,7 +16,7 @@ int main(int argc, char *argv[]) {
    fcntl(sockfd, F_SETFL, flags_socket);
 
    // make standard input non-blocking ...
-   int flags_stdin = fcntl(0, F_GETFL);
+   int flags_stdin = fcntl(STDIN_FILENO, F_GETFL);
    flags_stdin |= O_NONBLOCK;
    fcntl(STDIN_FILENO, F_SETFL, flags_stdin);
 
@@ -23,16 +24,8 @@ int main(int argc, char *argv[]) {
    servaddr.sin_family = AF_INET;
    servaddr.sin_addr.s_addr = INADDR_ANY;
 
-   // read port from standard input ...
+   // read port from argument ...
    servaddr.sin_port = htons(atoi(argv[1])); // Big endian
-
-   /*
-      connection has been established as soon as the client has sent over data. 
-      the server does not have to accept any other connections. 
-      once this connection is established, it will:
-      1. start reading from the socket and output its contents to standard output. 
-      2. start reading from standard input and forwarding its contents to the newly connected client.
-   */
 
    int did_bind = bind(sockfd, (struct sockaddr*) &servaddr, 
                      sizeof(servaddr));
@@ -49,9 +42,14 @@ int main(int argc, char *argv[]) {
                               // socket  store data  how much
                                  0, (struct sockaddr*) &clientaddr, 
                                  &clientsize);
-      if (bytes_recvd < 0) return errno;
       // check if client is connected ...
-      if (bytes_recvd = 0) continue;
+      if (bytes_recvd <= 0) {
+         if (errno != EAGAIN && errno != EWOULDBLOCK) {
+            return errno;
+         } else {
+            continue;
+         }
+      }
       
       // if data has been recieved (client connected) ...
       char* client_ip = inet_ntoa(clientaddr.sin_addr); // "Network bytes to address string"
@@ -61,9 +59,9 @@ int main(int argc, char *argv[]) {
 
       // read from stdin ...
       char server_buf[BUF_SIZE];
-      read(STDIN_FILENO, server_buf, BUF_SIZE);
+      ssize_t bytes_read = read(STDIN_FILENO, server_buf, BUF_SIZE);
       // if data available from stdn, send to client ...
-      int did_send = sendto(sockfd, server_buf, strlen(server_buf), 
+      int did_send = sendto(sockfd, server_buf, bytes_read, 
                         // socket  send data   how much to send
                            0, (struct sockaddr*) &clientaddr, 
                         // flags   where to send
